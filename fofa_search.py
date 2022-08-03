8f466fa12a117e1c440c8ec5c3442238b7dd64d4
dixin1 <dixin1@staff.weibo.com>
2022-08-03 17:50:29 +0800

    将fofa库整合到脚本中

--- fofa_search.py
+++ fofa_search.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 #-*- coding:utf-8 -*-
 
+import base64
+import json
+import urllib
 import fofa
 import math
 import time
@@ -14,13 +17,54 @@ def header():
 	print('\033[34m!  使用格式: python3 fofa_search.py    \033[0m')
 	print('\033[34m*-----------------------------------------*\033[0m')
 
+
+class Client:
+    def __init__(self,email,key):
+        self.email = email
+        self.key = key
+        self.base_url = "https://fofa.info"
+        self.search_api_url = "/api/v1/search/all"
+        self.login_api_url = "/api/v1/info/my"
+        self.get_userinfo() #check email and key
+
+    def get_userinfo(self):
+        api_full_url = "%s%s" % (self.base_url,self.login_api_url)
+        param = {"email":self.email,"key":self.key}
+        res = self.__http_get(api_full_url,param)
+        return json.loads(res)
+
+    def get_data(self,query_str,page=1,fields=""):
+        res = self.get_json_data(query_str,page,fields)
+        return json.loads(res)
+
+    def get_json_data(self,query_str,page=1,fields=""):
+        api_full_url = "%s%s" % (self.base_url,self.search_api_url)
+        param = {"qbase64":base64.b64encode(query_str),"email":self.email,"key":self.key,"page":page,"fields":fields}
+        res = self.__http_get(api_full_url,param)
+        return res
+
+
+    def __http_get(self,url,param):
+        param = urllib.parse.urlencode(param)
+        url = "%s?%s" % (url,param)
+        try:
+            req = urllib2.Request(url)
+            res = urllib2.urlopen(req).read()
+            if "errmsg" in res:
+                raise RuntimeError(res)
+        except urllib.error.URLError as e:
+            print("errmsg："+e.read())
+            raise e
+        return res
+
+
 class FoFa:
 	#初始化
 	def __init__(self):
-		self.mail="xxxxxxx@qq.com"
-		self.key="xxxxxxxxxxxxxx"
+		self.mail="18850620382@139.com"
+		self.key="569459458cb1c02335dbebe8edf162e6"
 		try:
-			self.api = fofa.Fofa(self.mail,self.key)
+			self.api = Client(self.mail,self.key)
 			#print('ok')
 		except Exception as e:
 			print('Error: {}'.format(e))
