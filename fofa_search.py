64276472071d390d2d707d7acdf58c838effb136
Fly-Wuhoo <56914048+Fly-Wuhoo@users.noreply.github.com>
2022-08-06 11:11:12 +0800

    Update fofa_search.py

--- fofa_search.py
+++ fofa_search.py
@@ -3,12 +3,12 @@
 
 import base64
 import json
-import urllib
 import fofa
 import math
 import time
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
+import urllib.request,urllib.error,urllib.parse
 
 
 def header():
@@ -19,43 +19,43 @@ def header():
 
 
 class Client:
-    def __init__(self,email,key):
-        self.email = email
-        self.key = key
-        self.base_url = "https://fofa.info"
-        self.search_api_url = "/api/v1/search/all"
-        self.login_api_url = "/api/v1/info/my"
-        self.get_userinfo() #check email and key
+	def __init__(self,email,key):
+		self.email = email
+		self.key = key
+		self.base_url = "https://fofa.info"
+		self.search_api_url = "/api/v1/search/all"
+		self.login_api_url = "/api/v1/info/my"
+		self.get_userinfo() #check email and key
 
-    def get_userinfo(self):
-        api_full_url = "%s%s" % (self.base_url,self.login_api_url)
-        param = {"email":self.email,"key":self.key}
-        res = self.__http_get(api_full_url,param)
-        return json.loads(res)
+	def get_userinfo(self):
+		api_full_url = "%s%s" % (self.base_url,self.login_api_url)
+		param = {"email":self.email,"key":self.key}
+		res = self.__http_get(api_full_url,param)
+		return json.loads(res)
 
-    def get_data(self,query_str,page=1,fields=""):
-        res = self.get_json_data(query_str,page,fields)
-        return json.loads(res)
+	def get_data(self,query_str,page=1,fields=""):
+		res = self.get_json_data(query_str,page,fields)
+		return json.loads(res)
 
-    def get_json_data(self,query_str,page=1,fields=""):
-        api_full_url = "%s%s" % (self.base_url,self.search_api_url)
-        param = {"qbase64":base64.b64encode(query_str),"email":self.email,"key":self.key,"page":page,"fields":fields}
-        res = self.__http_get(api_full_url,param)
-        return res
+	def get_json_data(self,query_str,page=1,fields=""):
+		api_full_url = "%s%s" % (self.base_url,self.search_api_url)
+		param = {"qbase64":base64.b64encode(query_str),"email":self.email,"key":self.key,"page":page,"fields":fields}
+		res = self.__http_get(api_full_url,param)
+		return res
 
 
-    def __http_get(self,url,param):
-        param = urllib.parse.urlencode(param)
-        url = "%s?%s" % (url,param)
-        try:
-            req = urllib2.Request(url)
-            res = urllib2.urlopen(req).read()
-            if "errmsg" in res:
-                raise RuntimeError(res)
-        except urllib.error.URLError as e:
-            print("errmsg："+e.read())
-            raise e
-        return res
+	def __http_get(self,url,param):
+		param = urllib.parse.urlencode(param)
+		url = "%s?%s" % (url,param)
+		try:
+			req = urllib.request.Request(url)
+			res = urllib.request.urlopen(req).read()
+			if "errmsg" in str(res):
+				raise RuntimeError(res)
+		except urllib.error.URLError as e:
+			print("errmsg："+e.read())
+			raise e
+		return res
 
 
 class FoFa:
@@ -161,9 +161,6 @@ class FoFa:
 		except Exception as e:
 			print('Error: {}'.format(e))
 		
-
-#def Search(dork):
-    
 
 if __name__ == '__main__':
 	header()
